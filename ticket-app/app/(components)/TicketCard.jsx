e5b54be256dac322b2804022e3fffd15ea8a60e1
Kawayan <kawayan.deguzman@gmail.com>
2024-01-04 23:59:03 -0800

    add p tag

--- ticket-app/app/(components)/TicketCard.jsx
+++ ticket-app/app/(components)/TicketCard.jsx
@@ -14,7 +14,9 @@ const TicketCard = () => {
       </div>
       <h4>Ticket Title</h4>
       <hr className="h-px border-0 bg-page mb-2"></hr>
-      <p className="whitespace-pre-wrap"></p>
+      <p className="whitespace-pre-wrap">
+        This is the ticket description! Please do this ticket{" "}
+      </p>
       <ProgressDisplay></ProgressDisplay>
       <StatusDisplay></StatusDisplay>
     </div>
