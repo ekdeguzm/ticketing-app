d56dac9935ce90648ea528a78a1c74b50c9739be
Kawayan <kawayan.deguzman@gmail.com>
2024-01-04 23:47:30 -0800

    add classes to ticketCard

--- ticket-app/app/(components)/TicketCard.jsx
+++ ticket-app/app/(components)/TicketCard.jsx
@@ -1,9 +1,21 @@
 import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = () => {
   return (
-    <div>
-      <DeleteBlock />
+    <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay></PriorityDisplay>
+        <div className="ml-auto">
+          <DeleteBlock />
+        </div>
+      </div>
+      <h4>Ticket Title</h4>
+      <hr></hr>
+      <ProgressDisplay></ProgressDisplay>
+      <StatusDisplay></StatusDisplay>
     </div>
   );
 };
